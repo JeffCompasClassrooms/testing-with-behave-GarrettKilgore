e9a77d31f89500a2bfb6cd1cc96b2938e3674242
Garrett Kilgore <garrettkilgore@DESKTOP-4P8F6C4.localdomain>
2025-11-22 20:45:08 -0700

    testing peppers

--- peppers-ghost/features/environment.py
+++ peppers-ghost/features/environment.py
@@ -2,9 +2,11 @@ import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-
-    context.behave_driver = behave_webdriver.Chrome.headless()
+    # Create a headless Chrome driver
+    driver = behave_webdriver.Chrome.headless()
+    # Attach it under both names for compatibility
+    context.behave_driver = driver
+    context.browser = driver
 
 def after_all(context):
-
-    context.behave_driver.quit()
+    context.browser.quit()
--- peppers-ghost/features/steps/peppers-steps.py
+++ peppers-ghost/features/steps/peppers-steps.py
@@ -2,22 +2,22 @@ from behave import given, when, then
 from selenium.webdriver.common.by import By
 from behave_webdriver.steps import *
 
-@given("I open the instructables peppers ghost page")
+@given('I open the instructables peppers ghost page')
 def step_open_page(context):
-    context.behave_driver.get("https://www.instructables.com/Peppers-Ghost-Illusion-in-a-Small-Space/")
+    context.browser.get("https://www.instructables.com/Peppers-Ghost-Illusion-in-a-Small-Space/")
 
-@then("I expect that there is at least one picture there")
+@then('I expect that there is at least one picture there')
 def step_check_for_picture(context):
-    images = context.behave_driver.find_elements(By.CSS_SELECTOR, "img") 
-    assert images != []
+    images = context.browser.find_elements(By.CSS_SELECTOR, "img")
+    assert images, "No images found on the page"
 
 @then('the page should contain the text "{text}"')
 def step_page_contains_text(context, text):
-    assert text.lower() in context.behave_driver.page_source.lower(), f'Text "{text}" not found on page.'
+    assert text.lower() in context.browser.page_source.lower(), f'Text "{text}" not found on page.'
 
 @then('the title should contain "{text}"')
 def step_title_contains_text(context, text):
-    assert text.lower() in context.behave_driver.title.lower(), f'Title does not contain "{text}".'
+    assert text.lower() in context.browser.title.lower(), f'Title does not contain "{text}".'
 
 @then('there should be no element with {locator_type} "{value}"')
 def step_no_element_by(context, locator_type, value):
@@ -30,12 +30,10 @@ def step_no_element_by(context, locator_type, value):
     }
     by = locator_map.get(locator_type)
     assert by, f'Unsupported locator type: {locator_type}'
-    elements = context.behave_driver.find_elements(by, value)
+    elements = context.browser.find_elements(by, value)
     assert len(elements) == 0, f'Element with {locator_type} "{value}" was found.'
 
 @when('I scroll to the section containing "{text}"')
 def step_scroll_to_section(context, text):
-    element = context.behave_driver.find_element(By.XPATH, f"//*[contains(text(), '{text}')]")
-    context.behave_driver.execute_script("arguments[0].scrollIntoView(true);", element)
-
-#
+    element = context.browser.find_element(By.XPATH, f"//*[contains(text(), '{text}')]")
+    context.browser.execute_script("arguments[0].scrollIntoView(true);", element)
