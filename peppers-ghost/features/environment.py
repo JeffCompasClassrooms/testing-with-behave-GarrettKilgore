076a93cece8469ef2179889c9a653a2d55576655
Garrett Kilgore <garrettkilgore@DESKTOP-4P8F6C4.localdomain>
2025-11-22 21:05:01 -0700

    Back to the Default

--- peppers-ghost/features/environment.py
+++ peppers-ghost/features/environment.py
@@ -1,14 +1,10 @@
-import os
 import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-    if os.getenv("BEHAVE_HEADLESS", "true").lower() == "true":
-        driver = behave_webdriver.Chrome.headless()
-    else:
-        driver = behave_webdriver.Chrome()
-    context.browser = driver
-    context.behave_driver = driver
+
+    context.behave_driver = behave_webdriver.Chrome.headless()
 
 def after_all(context):
-    context.browser.quit()
+
+    context.behave_driver.quit()
